7f0873d3224d120d8c258334863a2a24c6a368dd
elbeg9amar <elbeg9amar@yahoo.com>
2020-10-17 15:03:56 -0400

    added pool

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,11 @@ module.exports = {
       tableName: 'dbmigrations',
     },
     seeds: { directory: './database/seeds' },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
   },
 
   testing: {
